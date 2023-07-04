3aa5d1dff2f36e1d83ee1a629fe872a0c4e1279d
shiroichi28 <manikandanmk2972@gmail.com>
2023-07-04 20:13:30 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from "react";
 import Quote from "./components/Quote";
 import axios from "axios";
+
 function App() {
   const [quote, setQuote] = useState({
     anime: null,
     character: null,
     quote: null,
   });
+
   const getQuote = async () => {
-    return await fetch("https://animechan.xyz/api/random").then((response) =>
-      response.json()
-    );
-  };
-useEffect(() => {
-  const fetchQuote = async () => {
-    setQuote(await getQuote());
+    try {
+      const response = await axios.get("https://animechan.xyz/api/random");
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+      return null;
+    }
   };
 
-  fetchQuote();
-}, []);
+  const fetchQuote = async () => {
+    const newQuote = await getQuote();
+    if (newQuote) {
+      setQuote(newQuote);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
   return (
-
-      <div className="app">
-        <Quote quote={quote}/>
-        <button onClick={()=>setQuote(getQuote())}>Generate</button>
-      </div>
-    
+    <div className="app">
+      <Quote quote={quote} />
+      <button onClick={fetchQuote}>Generate</button>
+    </div>
   );
 }
 
